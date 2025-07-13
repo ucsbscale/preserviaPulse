57d3fd25e7759c0ba025d0481c5fe68f64155bd2
xueyan-jx <yanxue171@gmail.com>
2025-07-12 22:02:40 -0700

    reorganize code

--- code/06_postProccOcc.R
+++ code/06_postProccOcc.R
@@ -1,4 +1,6 @@
-## Purpose of script: make sure final plants and animals occ points have the same format for SSDM
+## Purpose of script:
+## 1. Make sure final plants and animals occ points have the same format for SSDM
+## 2. Add CNDDB data
 ## Authors: GEOG 274
 ## Date: Spring, 2025
 
@@ -67,7 +69,7 @@ write.table(Anim_Plant_merge, file.path('data/occurrences/Anim_Plant_merge.csv')
 
 
 ## ---------------------1.4 merge Anim_Plant_merge and data from CNDDB -------------
-CNDDB <- read.csv(here("data","occurrences","CNDDB_cleaned.csv"))
+CNDDB <- read.csv(here("data","occurrences","CNDDB_cleaned_7.5_updated.csv"))
 Anim_Plant_merge <- read.csv(here("data","occurrences","Anim_Plant_merge.csv"))
 
 CNDDB <- CNDDB %>%
@@ -84,7 +86,7 @@ CNDDB <- CNDDB %>%
 merged_df <- bind_rows(CNDDB, Anim_Plant_merge) %>%
   distinct(species, x, y, .keep_all = TRUE)
 
-write.table(merged_df, file.path('data/occurrences/Anim_Plant_merge_final0616.csv'), sep=',', row.names = FALSE)
+write.table(merged_df, file.path('data/occurrences/Anim_Plant_merge_final0711.csv'), sep=',', row.names = FALSE)
 
 ## -------------------- 1.5 upload to gdrive -----------------------
 drive_auth()
